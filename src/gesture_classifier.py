2f02bf62303f405bb2d9fc5c75c32bb95ac52d06
AmickGIT <amickpvt@gmail.com>
2025-01-17 18:51:08 +0530

    Few improvements

--- src/gesture_classifier.py
+++ src/gesture_classifier.py
@@ -14,21 +14,9 @@ class GestureClassifier:
         return exp_values / np.sum(exp_values)
 
     def classify(self, landmarks):
-        """
-        Classify gesture based on hand landmarks.
-
-        Simplified logic:
-        - "Closed Hand": All fingers are folded.
-        - "Open Hand": All fingers are extended.
-        - "Unknown Gesture": Any other configuration.
-        """
-        controller = MotionController()
+        
         index_tip = landmarks.landmark[8]
-        index_base = landmarks.landmark[5]
-        wrist = landmarks.landmark[0]
-        
-        
-
+       
         is_ring_closed = self.is_finger_closed(landmarks, finger_tip_index=16, finger_base_index = 13)
         is_middle_closed = self.is_finger_closed(landmarks, finger_tip_index=12, finger_base_index = 9)
         is_acclerating = self.is_acclerating(landmarks)
@@ -39,12 +27,6 @@ class GestureClassifier:
             direction = self.classify_vector_direction(index_vector)
             return f"{direction} and {is_acclerating}"
 
-            
-
-
-    
-
-
     def calculate_distance(self, coords1, coords2):
         return math.sqrt((coords1[0]-coords2[0])**2 + (coords1[1]-coords2[1])**2 + (coords1[2]-coords2[2])**2)
     
@@ -53,7 +35,7 @@ class GestureClassifier:
         finger_base = hand_landmarks.landmark[finger_base_index]
         distance = self.calculate_distance((finger_tip.x, finger_tip.y, finger_tip.z), (finger_base.x, finger_base.y, finger_tip.z))
         
-        return distance < 0.30
+        return distance < 0.35
     
     def is_acclerating(self, hand_landmarks, thumb_tip_index = 4, index_base_index = 5, middle_joint_index = 10):
         thumb_tip = hand_landmarks.landmark[thumb_tip_index]
@@ -74,11 +56,6 @@ class GestureClassifier:
 
         return direction/magnitude 
     
-    
-    
-
-    
-
     def classify_vector_direction(self, vector, threshold=0.20):
         directions = ["X-axis", "Y-axis", "Z-axis"]
         axis_labels = {
@@ -87,10 +64,8 @@ class GestureClassifier:
             "Z-axis": ["Back", "Front"],
         }
         
-        # vector = np.array(vector)
 
         softmaxed_vector = self.enhanced_softmax(np.abs(vector))
-        print(softmaxed_vector)                                             #DELETE
         
         # Find the dominant axis
         dominant_axis = np.argmax(softmaxed_vector)
